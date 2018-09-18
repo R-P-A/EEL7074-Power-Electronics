ff41f30dd4f8e4d4ffcfe5a7a9455f53b2c2c8a7
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2018-09-18 00:40:42 -0300

    Changed folder names, lab2 plotting all.
    Changed folder names,  .gitignore added pdf, lab2 is plotting fft also.

--- lab2.py
+++ lab2.py
@@ -3,13 +3,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-ch1 = pd.read_csv('Imagens/ALL0012/F0012CH1.CSV', usecols=[3,4], index_col=[0], header=None, names=['t', 'Vin (V)'])
-ch2 = pd.read_csv('Imagens/ALL0012/F0012CH2.CSV', usecols=[3,4], index_col=[0], header=None, names=['t', 'Iin (A*83.33)'])
-mth = pd.read_csv('Imagens/ALL0012/F0012MTH.CSV', usecols=[3,4], index_col=[0], header=None, names=['t', 'P (W)'])
-out = pd.concat([ch1,ch2,mth], axis=1)
-out['Iin (A*83.33)'] = out['Iin (A*83.33)']*83.33
-print(out.head())
-print(out.tail())
+# Plotting CH1, CH2 and MATH
+for i in [12,13,14,17,18]:
+    print("\n---------------- File: " + str(i) + " ----------------")
+    ch1 = pd.read_csv('src_img/lab2/ALL00'+str(i)+'/F00'+str(i)+'CH1.CSV', usecols=[3,4], index_col=[0], header=None, names=['t (s)', 'Vin (V)'])
+    ch2 = pd.read_csv('src_img/lab2/ALL00'+str(i)+'/F00'+str(i)+'CH2.CSV', usecols=[3,4], index_col=[0], header=None, names=['t (s)', 'Iin x 83.33 (A)'])
+    mth = pd.read_csv('src_img/lab2/ALL00'+str(i)+'/F00'+str(i)+'MTH.CSV', usecols=[3,4], index_col=[0], header=None, names=['t (s)', 'P (W)'])
+    out = pd.concat([ch1,ch2,mth], axis=1)
+    out['Iin x 83.33 (A)'] = out['Iin x 83.33 (A)']*83.33
+    print(out.head())
+    print(out.tail())
+    out.plot()
+    plt.savefig('img/lab2/plot' + str(i) + '.pdf')
 
-out.plot()
-plt.show()
+# Plotting FFT
+for i in [15,16,19,20]:
+    print("\n---------------- File: " + str(i) + " ----------------")
+    fft = pd.read_csv('src_img/lab2/ALL00'+str(i)+'/F00'+str(i)+'FFT.CSV', usecols=[3,4], index_col=[0], header=None, names=['f (Hz)', 'Módulo (dB)'])
+    print(fft.head())
+    print(fft.tail())
+    fft.plot()
+    plt.savefig('img/lab2/plot' + str(i) + '.pdf')
