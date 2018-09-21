a71ba1b123d34571282901060c8e2729f5663b82
Rafael Pintar Alevato <rpa.alevatO@gmail.com>
2018-09-21 14:54:05 -0300

    Calculating THD correctly.
    Changed sample number to match a frequency resolution of 60 Hz and adapted THD.

--- lab2.py
+++ lab2.py
@@ -56,44 +56,24 @@ def calculate_fourier(time, signal):
     freq_fourier = np.fft.fftfreq(signal.size, (time[1]-time[0]))
     return [freq_fourier, fourier]
 
-def calculate_thd(freq_fourier, fourier):
-    # Calculating Harmonics in multiple of 60 Hz
-    fourier_harmonics = []
-    counter = 0
-    for i in range(9):
-        if i % 2 == 0:
-            if (i == 0):
-                interpol = np.polyfit(freq_fourier[counter+1:counter+4], np.abs(fourier[counter+1:counter+4]), 2)
-                fourier_harmonic = interpol[0]*((60*(i+1))**2) + interpol[1]*(60*(i+1)) + interpol[2]
-                fourier_harmonics.append(fourier_harmonic)
-                print(np.abs(fourier[counter+1:counter+4]))
-                print(fourier_harmonics[i])
-            else:
-                interpol = np.polyfit(freq_fourier[counter+1:counter+3], np.abs(fourier[counter+1:counter+3]), 1)
-                fourier_harmonic = interpol[0]*(60*(i+1)) + interpol[1]
-                print(np.abs(fourier[counter+1:counter+3]))
-                print(fourier_harmonic)
-                fourier_harmonics.append(fourier_harmonic)
-        else:
-            counter = counter + 3
-            fourier_harmonics.append(np.abs(fourier[counter]))
-    
+def calculate_thd(fourier, precision):    
     # Calculando THD
     thd = 0
-    for i in range(1,9):
-        thd = thd + (fourier_harmonics[i])**2
-    thd = np.sqrt(thd) / fourier_harmonics[0]
+    for i in range(2,precision):
+        thd = thd + np.abs(fourier[i])**2
+    
+    thd = np.sqrt(thd) / np.abs(fourier[1])
     return thd
 
-time_index_period = 1667
+number_samples = 1667
 # Calculating the Frequency Spectrum of the Voltage Input for R
 time = ch1_fwrr.index.values
-time = time[0:time_index_period]
+time = time[0:number_samples]
 vin_r = ch1_fwrr['Vin (V)'].values
-vin_r = vin_r[0:time_index_period]
+vin_r = vin_r[0:number_samples]
 fft_vin_r = calculate_fourier(time, vin_r)
-fft_vin_r[1] = fft_vin_r[1] / time_index_period
-thd = calculate_thd(fft_vin_r[0], fft_vin_r[1])
+fft_vin_r[1] = fft_vin_r[1] / number_samples
+thd = calculate_thd(fft_vin_r[1], 20)
 print("THD: {}".format(thd))
 
 figure, plot = plt.subplots(2,1)
@@ -113,12 +93,12 @@ plt.savefig('img/lab2/vin_r_fourier.pdf')
 
 # Calculating the Frequency Spectrum of the Current Input for R load
 time = ch2_fwrr.index.values
-time = time[0:time_index_period]
+time = time[0:number_samples]
 iin_r = ch2_fwrr['Iin (A)'].values
-iin_r = iin_r[0:time_index_period]
+iin_r = iin_r[0:number_samples]
 fft_iin_r = calculate_fourier(time, iin_r)
-fft_iin_r[1] = fft_iin_r[1] / time_index_period
-thd = calculate_thd(fft_iin_r[0], fft_iin_r[1])
+fft_iin_r[1] = fft_iin_r[1] / number_samples
+thd = calculate_thd(fft_iin_r[1], 20)
 print("THD: {}".format(thd))
 
 figure, plot = plt.subplots(2,1)
@@ -138,12 +118,12 @@ plt.savefig('img/lab2/iin_r_fourier.pdf')
 
 # Calculating the Frequency Spectrum of the Voltage Input for RL
 time = ch1_fwrrl.index.values
-time = time[0:time_index_period]
+time = time[0:number_samples]
 vin_rl = ch1_fwrrl['Vin (V)'].values
-vin_rl = vin_rl[0:time_index_period]
+vin_rl = vin_rl[0:number_samples]
 fft_vin_rl = calculate_fourier(time, vin_rl)
-fft_vin_rl[1] = fft_vin_rl[1]/time_index_period
-thd = calculate_thd(fft_vin_rl[0], fft_vin_rl[1])
+fft_vin_rl[1] = fft_vin_rl[1]/number_samples
+thd = calculate_thd(fft_vin_rl[1], 20)
 print("THD: {}".format(thd))
 
 figure, plot = plt.subplots(2,1)
@@ -163,12 +143,12 @@ plt.savefig('img/lab2/vin_rl_fourier.pdf')
 
 # Calculating the Frequency Spectrum of the Current Input for RL load
 time = ch2_fwrrl.index.values
-time = time[0:time_index_period]
+time = time[0:number_samples]
 iin_rl = ch2_fwrrl['Iin (A)'].values
-iin_rl = iin_rl[0:time_index_period]
+iin_rl = iin_rl[0:number_samples]
 fft_iin_rl = calculate_fourier(time, iin_rl)
-fft_iin_rl[1] = fft_iin_rl[1]/time_index_period
-thd = calculate_thd(fft_iin_rl[0], fft_iin_rl[1])
+fft_iin_rl[1] = fft_iin_rl[1]/number_samples
+thd = calculate_thd(fft_iin_rl[1], 20)
 print("THD: {}".format(thd))
 
 figure, plot = plt.subplots(2,1)
