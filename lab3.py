cf4a4459136bd549c545a5f10a4dbc633772d218
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2018-10-09 08:17:47 -0300

    Added R and RL load to plot.
    Also added the function to plot mth with other channels.

--- lab3.py
+++ lab3.py
@@ -8,7 +8,7 @@ import pandas as pd
 # Saving Original Images based on CSV files of the Osciloscope
 # Plotting Original CH1, CH2 and MATH and saving for reference
 
-# General function for plotting originals. Returns the dataframe created with all channels.
+# General function for plotting originals ch1 and ch2. Returns the dataframe created with all channels.
 def plot_original_ch1_ch2(orig_path_ch1, orig_path_ch2, ch1_2_scale, ch1_name, ch2_name, out_path_name):
     # Importing data from source CSVs
     ch1 = pd.read_csv(orig_path_ch1, usecols=[3,4], index_col=[0], header=None, names=['t (s)', ch1_name])
@@ -26,36 +26,91 @@ def plot_original_ch1_ch2(orig_path_ch1, orig_path_ch2, ch1_2_scale, ch1_name, c
     plt.clf()
     return out
 
+# General function for plotting originals ch1, ch2 and mth. Returns the dataframe created with all channels.
+def plot_original_ch1_ch2_mth(orig_path_ch1, orig_path_ch2, orig_path_mth, ch1_2_scale, ch1_name, ch2_name, mth_name, total_path_name):
+    # Importing data from source CSVs
+    ch1 = pd.read_csv(orig_path_ch1, usecols=[3,4], index_col=[0], header=None, names=['t (s)', ch1_name])
+    ch2 = pd.read_csv(orig_path_ch2, usecols=[3,4], index_col=[0], header=None, names=['t (s)', ch2_name])
+    mth = pd.read_csv(orig_path_mth, usecols=[3,4], index_col=[0], header=None, names=['t (s)', mth_name])
+    # Correcting current for better scaling in image. Vscale/Iscale
+    total = pd.concat([ch1, ch2*ch1_2_scale, mth], axis=1)
+    # Plotting and saving the image
+    plt.plot(total.index, total[ch1_name])
+    plt.plot(total.index, total[ch2_name])
+    plt.plot(total.index, total[mth_name])
+    plt.xlabel('Tempo (s)')
+    plt.legend()
+    plt.grid(True)
+    plt.tight_layout()
+    plt.savefig(total_path_name)
+    plt.clf()
+    return total
 
-# Full Wave Rectfier with RL Load
-out_fwrrl_2_5ms = plot_original_ch1_ch2(
-    "src_img/lab3/ALL0022/F0022CH1.CSV",
-    "src_img/lab3/ALL0022/F0022CH2.CSV",
-    100,
+# Full Wave Rectfier Input with R Load
+fwrr_in = plot_original_ch1_ch2_mth (
+    'src_img/lab2/ALL0014/F0014CH1.CSV',
+    'src_img/lab2/ALL0014/F0014CH2.CSV',
+    'src_img/lab2/ALL0014/F0014MTH.CSV',
+    83.33,
     'Vin (V)',
-    'Iin (A) x 100',
-    'img/lab3/orig_rl_2,5ms.png'
+    'Iin (A) x 83,33',
+    'P (W)',
+    'img/lab3/orig_in_r.png'
 )
 
-# Full Wave Rectfier with RL Load
-out_fwrrl_5ms = plot_original_ch1_ch2(
+# Full Wave Rectfier Input with RL Load
+fwrrl_in = plot_original_ch1_ch2_mth (
+    'src_img/lab2/ALL0018/F0018CH1.CSV',
+    'src_img/lab2/ALL0018/F0018CH2.CSV',
+    'src_img/lab2/ALL0018/F0018MTH.CSV',
+    83.33,
+    'Vin (V)',
+    'Iin (A) x 83,33',
+    'P (W)',
+    'img/lab3/orig_in_rl.png'
+)
+
+# Full Wave Rectfier Output with RC Load
+fwrrc_out = plot_original_ch1_ch2(
     "src_img/lab3/ALL0023/F0023CH1.CSV",
     "src_img/lab3/ALL0023/F0023CH2.CSV",
     100,
     'Vin (V)',
     'Iin (A) x 100',
-    'img/lab3/orig_rl_5ms.png'
+    'img/lab3/orig_out_rc.png'
+)
+
+# Full Wave Rectfier Input with RC Load
+fwrrc_in = plot_original_ch1_ch2_mth(
+    "src_img/lab3/ALL0028/F0028CH1.CSV",
+    "src_img/lab3/ALL0028/F0028CH2.CSV",
+    "src_img/lab3/ALL0028/F0028MTH.CSV",
+    100,
+    'Vin (V)',
+    'Iin (A) x 100',
+    'P (W)',
+    'img/lab3/orig_in_rc.png'
 )
 
 # -------------------------------------------------------------------------------------------------
 
-# Calculating and Plotting Fourier Transforms
+# Calculating THD and Plotting Fourier Transforms
+
 
 # Function to calculate Fourier Transform
 def calculate_fft(time, signal):
     fourier = np.fft.fft(signal)
     freq_fourier = np.fft.fftfreq(signal.size, (time[1]-time[0]))
     return [freq_fourier, fourier]
+
+def calculate_thd(fourier, precision):    
+    # Calculando THD
+    thd = 0
+    for i in range(2,precision):
+        thd = thd + np.abs(fourier[i])**2
+    
+    thd = np.sqrt(thd) / np.abs(fourier[1])
+    return thd
 
 # Function to plot Original Function and Fourier Transform
 def plot_fourier_transform(number_samples, time, values, y_name, out_path_name):
@@ -69,40 +124,90 @@ def plot_fourier_transform(number_samples, time, values, y_name, out_path_name):
     # Plotting fft with original
     figure, plot = plt.subplots(2,1)
     plot[0].plot(time, values)
-    plot[0].set_title('Sinal Original Vin')
+    plot[0].set_title('Sinal Original')
     plot[0].set_xlabel('Tempo (s)')
     plot[0].set_ylabel(y_name)
+    if number_samples == 2500:
+        plot[1].set_xlim(-0.01,0.01)
     plot[0].grid(True)
     plot[1].stem(fft[0], np.abs(fft[1]))
     plot[1].set_title('Transformada Fourier')
     plot[1].set_xlabel('Frequência (Hz)')
     plot[1].set_ylabel(y_name)
-    plot[1].set_xlim(-1000,1000)
+    plot[1].set_xlim(0,800)
     plot[1].grid(True)
     plt.tight_layout()
     plt.savefig(out_path_name)
     return fft
 
-fft_vin_rl_2_5ms = plot_fourier_transform(
-    2500,
-    out_fwrrl_2_5ms.index.values,
-    out_fwrrl_2_5ms['Vin (V)'].values,
-    "Tensão (V)",
-    "img/lab3/vin_rl_2_5ms_2500_samples_fourier.png"
-)
-
-fft_vin_rl_2_5ms = plot_fourier_transform(
+# Full Wave Rectfier Voltage Input Fourier Transform with R Load
+fft_vin_r = plot_fourier_transform(
     1667,
-    out_fwrrl_2_5ms.index.values,
-    out_fwrrl_2_5ms['Vin (V)'].values,
+    fwrr_in.index.values,
+    fwrr_in['Vin (V)'].values,
     "Tensão (V)",
-    "img/lab3/vin_rl_2_5ms_1667_samples_fourier.png"
+    "img/lab3/vin_r_fourier.png"
 )
 
-fft_vin_rl_5ms = plot_fourier_transform(
-    2500,
-    out_fwrrl_5ms.index.values,
-    out_fwrrl_5ms['Vin (V)'].values,
+# Full Wave Rectfier Current Input Fourier Transform with R Load
+fft_iin_r = plot_fourier_transform(
+    1667,
+    fwrr_in.index.values,
+    fwrr_in['Iin (A) x 83,33'].values,
+    "Corrente (A)",
+    "img/lab3/iin_r_fourier.png"
+)
+
+# Full Wave Rectfier Voltage Input Fourier Transform with RL Load
+fft_vin_rl = plot_fourier_transform(
+    1667,
+    fwrrl_in.index.values,
+    fwrrl_in['Vin (V)'].values,
     "Tensão (V)",
-    "img/lab3/vin_rl_5ms_fourier.png"
+    "img/lab3/vin_rl_fourier.png"
+)
+
+# Full Wave Rectfier Current Input Fourier Transform with RL Load
+fft_iin_rl = plot_fourier_transform(
+    1667,
+    fwrrl_in.index.values,
+    fwrrl_in['Iin (A) x 83,33'].values,
+    "Corrente (A)",
+    "img/lab3/iin_rl_fourier.png"
+)
+
+# Full Wave Rectfier Voltage Input Fourier Transform with RC Load
+fft_vin_rc = plot_fourier_transform(
+    2500,
+    fwrrc_in.index.values,
+    fwrrc_in['Vin (V)'].values,
+    "Tensão (V)",
+    "img/lab3/vin_rc_fourier.png"
+)
+
+# Full Wave Rectfier Current Input Fourier Transform with RC Load
+fft_iin_rc = plot_fourier_transform(
+    2500,
+    fwrrc_in.index.values,
+    fwrrc_in['Iin (A) x 100'].values,
+    "Corrente (A)",
+    "img/lab3/iin_rc_fourier.png"
+)
+
+# Full Wave Rectfier Voltage Output Fourier Transform with RC Load
+fft_vout_rc = plot_fourier_transform(
+    2500,
+    fwrrc_out.index.values,
+    fwrrc_out['Vin (V)'].values,
+    "Tensão (V)",
+    "img/lab3/vout_rc_fourier.png"
+)
+
+# Full Wave Rectfier Current Output Fourier Transform with RC Load
+fft_iout_rc = plot_fourier_transform(
+    2500,
+    fwrrc_out.index.values,
+    fwrrc_out['Iin (A) x 100'].values,
+    "Corrente (A)",
+    "img/lab3/iout_rc_fourier.png"
 )
