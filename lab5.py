779c199ea3e4e9bf76288ebf18ddf8b437163e98
Rafael Pintar Alevato <rpa.alevato@gmail.com>
2018-10-30 23:04:12 -0300

    Added all the plots for lab5.

--- lab5.py
+++ lab5.py
@@ -10,15 +10,210 @@ import numeric_analysis as na
 
 # Plotting original images from oscilloscope
 
-orig_in_r = na.plot_oscope_original(
-    output_name = "Retificador de Onda Completa Trifásico Carga R - Entrada",
+orig_30_in_r = na.plot_oscope_original(
+    output_name = "Gradador com Alfa 30° e Carga R - Entrada",
     ch1_name = "Vin (V)",
     ch2_name = "Iin (A)",
     mth_name = "P (W)",
-    ch1_2_scale = 50,
+    ch1_2_scale = 100,
     xlimits = [-0.012, 0.012],
     orig_path_ch1 = "src_img/lab5/in_30_r_load/CH1.CSV",
     orig_path_ch2 = "src_img/lab5/in_30_r_load/CH2.CSV",
     orig_path_mth = "src_img/lab5/in_30_r_load/MTH.CSV",
     output_path = "img/lab5/orig_30_in_r.png"
 )
+
+orig_30_in_rl = na.plot_oscope_original(
+    output_name = "Gradador com Alfa 30° e Carga RL - Entrada",
+    ch1_name = "Vin (V)",
+    ch2_name = "Iin (A)",
+    mth_name = "P (W)",
+    ch1_2_scale = 100,
+    xlimits = [-0.012, 0.012],
+    orig_path_ch1 = "src_img/lab5/in_30_rl_load/CH1.CSV",
+    orig_path_ch2 = "src_img/lab5/in_30_rl_load/CH2.CSV",
+    orig_path_mth = "src_img/lab5/in_30_rl_load/MTH.CSV",
+    output_path = "img/lab5/orig_30_in_rl.png"
+)
+
+orig_60_in_r = na.plot_oscope_original(
+    output_name = "Gradador com Alfa 60° e Carga R - Entrada",
+    ch1_name = "Vin (V)",
+    ch2_name = "Iin (A)",
+    mth_name = "P (W)",
+    ch1_2_scale = 100,
+    xlimits = [-0.012, 0.012],
+    orig_path_ch1 = "src_img/lab5/in_60_r_load/CH1.CSV",
+    orig_path_ch2 = "src_img/lab5/in_60_r_load/CH2.CSV",
+    orig_path_mth = "src_img/lab5/in_60_r_load/MTH.CSV",
+    output_path = "img/lab5/orig_60_in_r.png"
+)
+
+orig_60_in_rl = na.plot_oscope_original(
+    output_name = "Gradador com Alfa 60° e Carga RL - Entrada",
+    ch1_name = "Vin (V)",
+    ch2_name = "Iin (A)",
+    mth_name = "P (W)",
+    ch1_2_scale = 100,
+    xlimits = [-0.012, 0.012],
+    orig_path_ch1 = "src_img/lab5/in_60_rl_load/CH1.CSV",
+    orig_path_ch2 = "src_img/lab5/in_60_rl_load/CH2.CSV",
+    orig_path_mth = "src_img/lab5/in_60_rl_load/MTH.CSV",
+    output_path = "img/lab5/orig_60_in_rl.png"
+)
+
+orig_30_out_r = na.plot_oscope_original(
+    output_name = "Gradador com Alfa 30° e Carga R - Entrada",
+    ch1_name = "Vin (V)",
+    ch2_name = "Iin (A)",
+    ch1_2_scale = 100,
+    xlimits = [-0.012, 0.012],
+    orig_path_ch1 = "src_img/lab5/out_30_r_load/CH1.CSV",
+    orig_path_ch2 = "src_img/lab5/out_30_r_load/CH2.CSV",
+    output_path = "img/lab5/orig_30_out_r.png"
+)
+
+orig_30_out_rl = na.plot_oscope_original(
+    output_name = "Gradador com Alfa 30° e Carga RL - Entrada",
+    ch1_name = "Vin (V)",
+    ch2_name = "Iin (A)",
+    ch1_2_scale = 100,
+    xlimits = [-0.012, 0.012],
+    orig_path_ch1 = "src_img/lab5/out_30_rl_load/CH1.CSV",
+    orig_path_ch2 = "src_img/lab5/out_30_rl_load/CH2.CSV",
+    output_path = "img/lab5/orig_30_out_rl.png"
+)
+
+orig_60_out_r = na.plot_oscope_original(
+    output_name = "Gradador com Alfa 60° e Carga R - Entrada",
+    ch1_name = "Vin (V)",
+    ch2_name = "Iin (A)",
+    ch1_2_scale = 100,
+    xlimits = [-0.012, 0.012],
+    orig_path_ch1 = "src_img/lab5/out_60_r_load/CH1.CSV",
+    orig_path_ch2 = "src_img/lab5/out_60_r_load/CH2.CSV",
+    output_path = "img/lab5/orig_60_out_r.png"
+)
+
+orig_60_out_rl = na.plot_oscope_original(
+    output_name = "Gradador com Alfa 60° e Carga RL - Entrada",
+    ch1_name = "Vin (V)",
+    ch2_name = "Iin (A)",
+    ch1_2_scale = 100,
+    xlimits = [-0.012, 0.012],
+    orig_path_ch1 = "src_img/lab5/out_60_rl_load/CH1.CSV",
+    orig_path_ch2 = "src_img/lab5/out_60_rl_load/CH2.CSV",
+    output_path = "img/lab5/orig_60_out_rl.png"
+)
+
+# -------------------------------------------------------------------------------------------------
+
+# Calculating THD and Plotting Fourier Transforms
+
+# Input 30° R Load
+fft_vin_30_r = na.plot_fourier_transform(
+    number_samples = 2500,
+    time = orig_30_in_r.index.values,
+    values = orig_30_in_r["Vin (V)"].values,
+    y_name = "Tensão (V)",
+    title = "Tensão de Entrada do Gradador com Alfa 30° e Carga R",
+    out_path_name = "img/lab5/fft_vin_30_r.png",
+    xlimits0 = [-0.012, 0.012],
+    xlimits1 = [0, 800]
+)
+THD = na.calculate_thd(fft_vin_30_r[0], fft_vin_30_r[1], 60, 100)
+print("\nTHD for Voltage Alfa 30° R Load: %s" % (THD))
+
+fft_iin_30_r = na.plot_fourier_transform(
+    number_samples = 2500,
+    time = orig_30_in_r.index.values,
+    values = orig_30_in_r["Iin (A)"].values,
+    y_name = "Corrente (A)",
+    title = "Corrente de Entrada do Gradador com Alfa 30° e Carga R",
+    out_path_name = "img/lab5/fft_iin_30_r.png",
+    xlimits0 = [-0.012, 0.012],
+    xlimits1 = [0, 800]
+)
+THD = na.calculate_thd(fft_iin_30_r[0], fft_iin_30_r[1], 60, 100)
+print("THD for Current Alfa 30° R Load: %s" % (THD))
+
+# Input 30° RL Load
+fft_vin_30_rl = na.plot_fourier_transform(
+    number_samples = 2500,
+    time = orig_30_in_rl.index.values,
+    values = orig_30_in_rl["Vin (V)"].values,
+    y_name = "Tensão (V)",
+    title = "Tensão de Entrada do Gradador com Alfa 30° e Carga RL",
+    out_path_name = "img/lab5/fft_vin_30_rl.png",
+    xlimits0 = [-0.012, 0.012],
+    xlimits1 = [0, 800]
+)
+THD = na.calculate_thd(fft_vin_30_rl[0], fft_vin_30_rl[1], 60, 100)
+print("\nTHD for Voltage Alfa 30° RL Load: %s" % (THD))
+
+fft_iin_30_rl = na.plot_fourier_transform(
+    number_samples = 2500,
+    time = orig_30_in_rl.index.values,
+    values = orig_30_in_rl["Iin (A)"].values,
+    y_name = "Corrente (A)",
+    title = "Corrente de Entrada do Gradador com Alfa 30° e Carga RL",
+    out_path_name = "img/lab5/fft_iin_30_rl.png",
+    xlimits0 = [-0.012, 0.012],
+    xlimits1 = [0, 800]
+)
+THD = na.calculate_thd(fft_iin_30_rl[0], fft_iin_30_rl[1], 60, 100)
+print("THD for Current Alfa 30° RL Load: %s" % (THD))
+
+# Input 60° R Load
+fft_vin_60_r = na.plot_fourier_transform(
+    number_samples = 2500,
+    time = orig_60_in_r.index.values,
+    values = orig_60_in_r["Vin (V)"].values,
+    y_name = "Tensão (V)",
+    title = "Tensão de Entrada do Gradador com Alfa 60° e Carga R",
+    out_path_name = "img/lab5/fft_vin_60_r.png",
+    xlimits0 = [-0.012, 0.012],
+    xlimits1 = [0, 800]
+)
+THD = na.calculate_thd(fft_vin_60_r[0], fft_vin_60_r[1], 60, 100)
+print("\nTHD for Voltage Alfa 60° R Load: %s" % (THD))
+
+fft_iin_60_r = na.plot_fourier_transform(
+    number_samples = 2500,
+    time = orig_60_in_r.index.values,
+    values = orig_60_in_r["Iin (A)"].values,
+    y_name = "Corrente (A)",
+    title = "Corrente de Entrada do Gradador com Alfa 60° e Carga R",
+    out_path_name = "img/lab5/fft_iin_60_r.png",
+    xlimits0 = [-0.012, 0.012],
+    xlimits1 = [0, 800]
+)
+THD = na.calculate_thd(fft_iin_60_r[0], fft_iin_60_r[1], 60, 100)
+print("THD for Current Alfa 60° R Load: %s" % (THD))
+
+# Input 60° RL Load
+fft_vin_60_rl = na.plot_fourier_transform(
+    number_samples = 2500,
+    time = orig_60_in_rl.index.values,
+    values = orig_60_in_rl["Vin (V)"].values,
+    y_name = "Tensão (V)",
+    title = "Tensão de Entrada do Gradador com Alfa 60° e Carga RL",
+    out_path_name = "img/lab5/fft_vin_60_rl.png",
+    xlimits0 = [-0.012, 0.012],
+    xlimits1 = [0, 800]
+)
+THD = na.calculate_thd(fft_vin_60_rl[0], fft_vin_60_rl[1], 60, 100)
+print("\nTHD for Voltage Alfa 60° RL Load: %s" % (THD))
+
+fft_iin_60_rl = na.plot_fourier_transform(
+    number_samples = 2500,
+    time = orig_60_in_rl.index.values,
+    values = orig_60_in_rl["Iin (A)"].values,
+    y_name = "Corrente (A)",
+    title = "Corrente de Entrada do Gradador com Alfa 60° e Carga RL",
+    out_path_name = "img/lab5/fft_iin_60_rl.png",
+    xlimits0 = [-0.012, 0.012],
+    xlimits1 = [0, 800]
+)
+THD = na.calculate_thd(fft_iin_60_rl[0], fft_iin_60_rl[1], 60, 100)
+print("THD for Current Alfa 60° RL Load: %s" % (THD))
